cb313db79e461d4f31e9214f3b7e6e5e4e2ea784
Abir Ahsan <abirahsan122@gmail.com>
2022-05-31 00:13:32 +0600

    add loading and api handle error on controller

--- lib/controller/quiz_contoller.dart
+++ lib/controller/quiz_contoller.dart
@@ -1,6 +1,11 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:quiz/services/custom_eassy_loading.dart';
 
 import '../model/question_model.dart';
+import '../services/api_error_handle_service.dart';
 import '../services/api_service/quiz_api_service.dart';
 
 class QuizController extends GetxController {
@@ -18,13 +23,21 @@ class QuizController extends GetxController {
 //<<======================================== Fetch All Question And Assign To List
   Future<void> fetchAllQuestionList() async {
     try {
+      CustomEassyLoading.startLoading();
       QuizApiService().getQuestionList().then((resp) {
         allQuestionList.value = resp.toList();
+        CustomEassyLoading.stopLoading();
       }, onError: (err) {
         print(err);
+        ApiErrorHandleService.handleStatusCodeError(err);
+        CustomEassyLoading.stopLoading();
       });
+    } on SocketException catch (e) {
+      debugPrint(e.toString());
+      CustomEassyLoading.stopLoading();
     } catch (e) {
       print(e);
+      CustomEassyLoading.stopLoading();
     }
     update();
   }
